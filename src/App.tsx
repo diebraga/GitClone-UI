57ca762be869db65b2abcde3ee896585365ea00f
unknown <diebraga0055@gmail.com>
2020-08-23 15:39:13 +0100

    all structure add Routes + Profile and styles + Repo + styles + Header and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from '../src/components/Header';
+import Profile from '../src/pages/Profile';
+import Repo from '../src/pages/Repo';
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+    <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />}/>
+        <Route path="/:username" element={<Profile />}/>
+        <Route path="/:username/:reponame" element={<Repo />}/>
+      </Routes>
+
+    {/*<Footer />*/}
+    </BrowserRouter>
   );
 }
 
