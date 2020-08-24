49eb59a771068d7dc355862bee2cd6ae44ed0b51
unknown <diebraga0055@gmail.com>
2020-08-24 21:20:34 +0100

    some profile and profiledata styles added

--- src/pages/Profile/styles.tsx
+++ src/pages/Profile/styles.tsx
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  padding-top: 24px;
+  padding-bottom: 24px;
+
+  padding-left: 16px;
+  padding-right: 16px;
+
 `;
 
 export const Main = styled.div`
-  
+  display: flex;
+  flex-direction: column;
 `;
 
 export const LeftSide = styled.div`
-  
+  @media (min-width: 768px) {
+    width: 25%
+  }
 `;
 
 export const RightSide = styled.div`
-  
+    @media (min-width: 768px) {
+    width: 75%
+  }
 `;
